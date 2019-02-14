da81fb47fd05dfd2a03ad596fcb6f1d76ef29886
José Augusto Mendes <zeca.saitou@gmail.com>
2019-02-14 13:52:33 -0200

    Updating Project
    Update Project.

---
+++ LayoutsInterface/obj/Debug/81/android/src/md57df9c95f8cf66fd9d0a6a3c40ebe4d5e/MyListView.java
@@ -0,0 +1,47 @@
+package md57df9c95f8cf66fd9d0a6a3c40ebe4d5e;
+
+
+public class MyListView
+	extends android.app.ListActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("LayoutsInterface.MyListView, LayoutsInterface", MyListView.class, __md_methods);
+	}
+
+
+	public MyListView ()
+	{
+		super ();
+		if (getClass () == MyListView.class)
+			mono.android.TypeManager.Activate ("LayoutsInterface.MyListView, LayoutsInterface", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
